380953514beb61bb287d5a0e0e4aa4ce9173a6ff
msutaria <msutaria@users.noreply.github.com>
2014-05-05 12:38:56 +0530

    changes for cordova v 3.1

--- src/android/com/appgyver/plugin/Echo.java
+++ src/android/com/appgyver/plugin/Echo.java
@@ -1,8 +1,8 @@
 package com.appgyver.plugin;
 
-import org.apache.cordova.api.CordovaPlugin;
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,7 +12,7 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class Echo extends CordovaPlugin {
-    @Override
+    
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("echo")) {
             String message = args.getString(0);
